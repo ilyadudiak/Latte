f36922d1f2780f408baf42de138a573052862b99
Ilya <61752920+ilyadudiak@users.noreply.github.com>
2022-12-28 16:42:09 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,3 @@
+let burger = document.querySelector(".burger");
+let burgerMenu = document.querySelector(".burger__menu");
+burger.addEventListener("click", ()=> burgerMenu.classList.toggle("showMenu"))

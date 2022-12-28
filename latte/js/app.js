8404275cd640b90212f3e06ea71cac67261cd5ad
Ilya <61752920+ilyadudiak@users.noreply.github.com>
2022-12-28 16:41:49 +0200

    Delete latte directory

--- latte/js/app.js
+++
@@ -1,3 +0,0 @@
-let burger = document.querySelector(".burger");
-let burgerMenu = document.querySelector(".burger__menu");
-burger.addEventListener("click", ()=> burgerMenu.classList.toggle("showMenu"))
